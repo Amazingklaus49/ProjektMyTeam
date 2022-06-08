edab585b75ad7f2273556d4c4d608f54aa6a072c
marc <sutter.marc49gmail.com>
2022-06-08 09:33:56 +0200

    a

---
+++ myteambackend/src/main/java/com/example/myteambackend/MyteambackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.myteambackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyteambackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyteambackendApplication.class, args);
+    }
+
+}
